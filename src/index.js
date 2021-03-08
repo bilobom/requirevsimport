3318b0a6c75ae85899dfb796a0623267b93538d4
bilal.boumaad <bilobom124@gmail.com>
2021-03-08 19:37:53 +0100

    add import vs require statement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+/* require vs import  */
+
+// using require
+const ReactDOM = require('react-dom')
+
+// using import: uncommnet to test
+// import ReactDOM from 'react-dom'
+
+const ReactDom = require('react-dom')
+
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
